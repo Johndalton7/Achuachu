90f1028df378c4b3362d93207d206e816496e5e4
Johndalton7 <86659836+Johndalton7@users.noreply.github.com>
2021-07-09 09:19:02 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -7,7 +7,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQ
 from bot import Translation # pylint: disable=import-error
 from bot.database import Database # pylint: disable=import-error
 from pyrogram.errors import UserNotParticipant
-from bot import FORCESUB_CHANNEL
+from bot import MFMOVIES3
 
 db = Database()
 
